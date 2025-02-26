8dc8e8f4d3fa0a160e9a77d554546ba626ebbfc1
CleverHipp0 <lilec3056@masseyhigh.school.nz>
2025-02-27 10:26:55 +1300

    Instructions function

---
+++ C_03_instructions.py
@@ -0,0 +1,46 @@
+def statement_generator(decoration, decoration_time, statement):
+    """Makes a statement fancy by adding decorative characters"""
+
+    print(f"\n{decoration * decoration_time} {statement} {decoration * decoration_time}\n")
+
+
+def yes_no(question):
+    """Input question, out put whether they said yes or no"""
+    while True:
+
+        # Asks a yes or no question
+        yes_no_raw = input(question).lower()
+
+        # acceptable answers in lists
+        yes_acceptable = ("yes", "y")
+        no_acceptable = ("no", "n")
+
+        # Checks the list to see if it's an acceptable answer and returns it
+        if yes_no_raw in yes_acceptable:
+            return yes_acceptable[0]
+
+        elif yes_no_raw in no_acceptable:
+            return no_acceptable[0]
+
+        else:
+            print("That is not an acceptable answer. Please answer with <yes> or <no>.")\
+
+
+
+def instructions():
+
+    want_instructions = yes_no("Do you want instructions? ")
+
+    if want_instructions == "yes":
+
+        statement_generator("*", 3, "Instructions")
+        print('''- 1
+- 2
+- 3
+        ''')
+
+
+# Main routine goes here
+
+instructions()
+
